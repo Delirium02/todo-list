0643b013c503dc4835e7799d9abe06081f0e691b
delirium02 <darknightbs@gmail.com>
2026-02-20 00:17:57 +1100

    Implemented functionality to show/hide add task form

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,25 @@
 import { tasksList, newTask } from "./tasks.js";
 import { createProject, defaultProject, projectManager, addTaskToProject } from "./projects.js"
+import "./style.css";
 
+const toggleBtnAdd = document.querySelector(".toggle-btn-add");
+const toggleBtnCancel = document.querySelector(".toggle-btn-cancel");
+const inputFieldToggle = document.querySelector(".input-field-toggle");
+
+toggleBtnCancel.style.display = "none";
+
+toggleBtnAdd.addEventListener("click", () => {
+    toggleBtnAdd.style.display = "none";
+    inputFieldToggle.style.display = "block";
+    toggleBtnCancel.style.display = "block";
+});
+
+toggleBtnCancel.addEventListener("click", () => {
+    toggleBtnAdd.style.display = "block";
+    inputFieldToggle.style.display = "none";
+    toggleBtnCancel.style.display = "none";
+})
+
+
+
+export { toggleBtnAdd };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { tasksList, newTask } from "./tasks.js";
 import { createProject, defaultProject, projectManager, addTaskToProject } from "./projects.js"
-
-
+import { toggleBtn } from "./dom.js";
+import "./style.css";
 
